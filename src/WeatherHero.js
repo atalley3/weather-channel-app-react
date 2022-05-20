bf85172e9a1ad56229437f9af842b5fd9112a659
atalley3 <ashley.l.talley@gmail.com>
2022-05-20 13:22:37 -0400

    Update WeatherHero.js
    added content to Weather Hero

--- src/WeatherHero.js
+++ src/WeatherHero.js
@@ -3,7 +3,28 @@ import React from "react";
 export default function WeatherHero() {
   return (
     <section className="WeatherHero">
-      <h1>Hello from Weather Hero</h1>
+      <header>
+        <h1>Asheville as of 1:00 pm on 5/20/22</h1>
+      </header>
+      <main className="container">
+        <div className="row">
+          <div className="col-md-8">
+            <ul>
+              <li>90 degrees F</li>
+              <li>Sunny and Hot AF</li>
+              <div className="d-flex">
+                <li>
+                  <span>Day Temp</span> 95
+                </li>
+                <li>
+                  <span>NightTemp</span> 65
+                </li>
+              </div>
+            </ul>
+          </div>
+          <div className="col-md-4">icon</div>
+        </div>
+      </main>
     </section>
   );
 }
