c157ef91e4e6427a707e75c5c9a080c4b74c3265
atalley3 <ashley.l.talley@gmail.com>
2022-05-19 20:45:43 -0400

    Create Footer.js

---
+++ src/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <a
+        href="https://www.github.com/atalley3/weather-channel-app"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Open source code
+      </a>{" "}
+      by{" "}
+      <a
+        href="https://https://www.linkedin.com/in/ashley-talley-41271950/"
+        target="_blank"
+        rel="noreferrer"
+      ></a>
+      Ashley Talley
+    </footer>
+  );
+}
+export default Footer;
